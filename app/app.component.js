5159c0d61be925966553494569eab17f0d9f4b27
Jean Paul Lasy <jp.lasy@gmail.com>
2016-10-06 22:34:14 +0000

    Update component files

--- app/app.component.js
+++ app/app.component.js
@@ -15,7 +15,7 @@ var AppComponent = (function () {
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: '<h1>PimpMyTrace</h1>'
+            templateUrl: './component.home.html'
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
-  template: '<h1>PimpMyTrace</h1>'
+  templateUrl: './component.home.html'
 })
 export class AppComponent { }
